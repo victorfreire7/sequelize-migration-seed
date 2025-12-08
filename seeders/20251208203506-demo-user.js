82cbab8774f75a799c4b3d716a7e6c810e02a793
victorfreire7 <vhugo.freire159@gmail.com>
2025-12-08 18:27:49 -0300

    initial commit

---
+++ seeders/20251208203506-demo-user.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [
+      {
+        firstName: 'Victor',
+        lastName: 'Hugo',
+        email: 'example@example.com',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
